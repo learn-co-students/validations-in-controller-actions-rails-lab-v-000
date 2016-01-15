04cfced2e588f81d2680a142bb2b53aa0e7572f5
onyoo <roberto9m9@gmail.com>
2016-01-15 12:02:20 +0100

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,10 +6,18 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render "edit"
+    end
   end
 
   private
 
   def post_params
+    params.permit(:title,:content,:category)
   end
 end
