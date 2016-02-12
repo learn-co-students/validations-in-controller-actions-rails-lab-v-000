28004bf70adadded950d55b849c25ede4b60d483
eric-an <erican@me.com>
2016-02-11 18:39:37 -0800

    create controller validations

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,10 +1,23 @@
 class AuthorsController < ApplicationController
   def show
+    @author = Author.find(params[:id])
   end
 
   def new
   end
 
   def create
+    @author = Author.new(post_params)
+    if @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
+
+private
+
+  def post_params
+    params.permit(:name, :email)
   end
 end
