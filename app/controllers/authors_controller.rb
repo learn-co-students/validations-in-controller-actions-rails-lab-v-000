d6c39305762c3fbab3c0a5286bb2b1385788a618
HarlemSquirrel <wittyman37@yahoo.com>
2016-02-17 15:11:20 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,5 +6,19 @@ class AuthorsController < ApplicationController
   end
 
   def create
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def author_params
+    #binding.pry
+    {name: params[:name], email: params[:email]}
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,10 +6,19 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
 
   def post_params
+    {title: params[:title], category: params[:category], content: params[:content]}
   end
 end
