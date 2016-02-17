1f91dd54a4044b747f6bba44e29cebee1ba4f641
febbraiod <donedesign1@gmail.com>
2016-02-17 14:50:18 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,10 +1,23 @@
 class AuthorsController < ApplicationController
-  def show
-  end
-
+  
   def new
+    @author = Author.new
   end
 
   def create
+    @author = Author.new(params.permit(:name, :email))
+
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+
   end
+
+  def show
+    @author = Author.find(params[:id])
+  end
+
 end
