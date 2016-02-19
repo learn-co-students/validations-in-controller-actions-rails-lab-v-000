c5ac3a14d58696058677dca637de47bd30f5e5ca
empireofryan <rjohnson2001@gmail.com>
2016-02-18 20:39:46 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :dry_post, only: [:show, :edit, :update]
   def show
   end
 
@@ -6,10 +7,21 @@ class PostsController < ApplicationController
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
+
 
   private
 
   def post_params
+    params.permit(:title, :category, :content)
+  end
+
+  def dry_post
+    @post = Post.find(params[:id])
   end
 end
