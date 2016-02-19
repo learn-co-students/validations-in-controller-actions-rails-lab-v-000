0aecf0346577d1f99a5be26f060cca5fd8d97fe1
preetdha <manpreet.dha@gmail.com>
2016-02-18 17:15:11 -0800

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+
+  before_action :post_params, only: [:show, :edit, :update]
+
   def show
   end
 
@@ -6,10 +9,17 @@ class PostsController < ApplicationController
   end
 
   def update
+
+     if @post.update(title: params[:title], category: params[:category], content: params[:content])
+      redirect_to @post
+    else
+      render :edit
+    end
   end
 
   private
 
   def post_params
+    @post = Post.find(params[:id])
   end
 end
