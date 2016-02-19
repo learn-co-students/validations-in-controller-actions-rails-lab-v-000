8dbabaabd1f615d95e5bc083c75f5c7ac835bde1
Chris Aporta <christopher.aporta@gmail.com>
2016-02-19 00:28:48 -0500

    complete lab

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,5 +6,18 @@ class AuthorsController < ApplicationController
   end
 
   def create
+    @author = Author.new(post_params)
+
+    if @author.save
+      redirect_to author_path @author
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.permit(:name, :email)
   end
 end
