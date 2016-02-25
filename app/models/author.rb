5e922bb14635d9dbdc761840c46b4f1ddcd7b403
jaeku <ja.ku@rutgers.edu>
2016-02-25 18:10:29 +0000

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true #Checks for name
+  validates :email, uniqueness: true #Checks for unique email
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true #Checks for title
+  validates :content, length: { minimum: 100 } #Content to be at least 100 characters long
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)} #Should include one of the following categories
 end
+
+# %w(string string) is a word array
