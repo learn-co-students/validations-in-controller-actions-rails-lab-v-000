60bee5f85dda75642957807cbe1ad0d488339db9
macgeek30 <mike25ko@gmail.com>
2016-02-25 18:07:04 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,11 +5,30 @@ class PostsController < ApplicationController
   def edit
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
 
   def post_params
+    params.permit(:title, :content, :category)
   end
 end
