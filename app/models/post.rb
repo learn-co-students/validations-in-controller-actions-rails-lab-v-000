99693667d4debcdddcfb105e1c3083f217138faa
Alvinluipo <lualvin1234@gmail.com>
2016-03-08 16:17:33 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validates :content, length: { minimum: 200 }
+  validate :params_are_valid?
+
+  def params_are_valid?
+    if !self.title.present? || !self.content.present? || !self.category.present?
+      errors.add(:title, 'Title cannot be blank!')
+      errors.add(:content, 'Content cannot be blank!')
+      errors.add(:category, 'Category cannot be blank!')
+    end
+  end
 end
