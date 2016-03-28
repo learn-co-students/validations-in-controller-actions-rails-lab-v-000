401801abba9983a0621962a65ee21e153098404a
esimonian <eli.simonian@gmail.com>
2016-03-28 12:01:17 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,10 +7,16 @@ class PostsController < ApplicationController
   def edit
   end
 
+
   def update
     @post.update(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
 
-    redirect_to post_path(@post)
   end
 
   private
