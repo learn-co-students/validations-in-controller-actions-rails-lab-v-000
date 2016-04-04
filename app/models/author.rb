a701d9d322b28c61638cf74c414b1ab44e56db27
Lady Codington <myemailforcoding@gmail.com>
2016-04-04 13:08:28 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+  validates(:name, { :length => { :minimum => 1 } })
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i,
+                      message: "not a valid email" }
 end
