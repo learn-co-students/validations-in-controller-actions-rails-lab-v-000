346149bd70359b2eb392ee2f4c3532c53403adcb
cernanb <cernanb@gmail.com>
2016-04-09 22:07:08 -0700

    created validations in controller action

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
+class GoodnessValidator < ActiveModel::Validator
+  def validate(record)
+    if record.category != "Fiction" && record.category != "Non-Fiction"
+      record.errors[:category] << "Category needs to be Fiction or Non-Fiction"
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :category, format: {with: /Fiction| Non-Fiction/}
+  validates :content, length: {minimum: 100}
+  validates_with GoodnessValidator
 end
