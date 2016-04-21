e8ef918d4c3decfcceaac7919fbf5e89da29e30f
glee38 <glee3388@gmail.com>
2016-04-21 19:05:56 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+  validates_presence_of :name
+  validates_presence_of :email
+  validates_uniqueness_of :email
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates_presence_of :title
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} must be either Fiction or Non-Fiction"}
+  validates :content, length: { minimum: 100}
 end
