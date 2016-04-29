180b3a1f9e53d6daf50f776a3adeb214966ffb19
lawrend <douglaslawrence314@gmail.com>
2016-04-29 13:44:53 -0700

    fixed posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,7 @@ class PostsController < ApplicationController
 
   def update
 
-    if Post.new(post_params).valid?
-      @post.update(post_params)
+    if @post.update(post_params)
       redirect_to post_path(@post)
     else
       render :edit
