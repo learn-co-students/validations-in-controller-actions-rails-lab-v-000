c08a8bb365cd7af8ce9dba3d94f3f53a59d46e0e
m-walker <michaelwalker610@gmail.com>
2016-06-17 17:00:03 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,12 +5,19 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    
     @post.update(post_params)
 
-    redirect_to post_path(@post)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
