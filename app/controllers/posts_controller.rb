9b3f8e00769ab0b0e2426002cbc9c97be8ebf6b4
DEREJE GEMECHU <dereje88@gmail.com>
2016-07-13 11:26:25 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,13 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    @tmp_post = Post.new(post_params)
+    if @tmp_post.valid?
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
