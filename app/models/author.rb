5bcd7e7428bba5778804354bf469dc2a21b11d1c
smithWEBtek <brad@smithwebtek.com>
2016-09-25 10:24:56 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,4 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
-  validates :email, presence: true
-  validates :email, uniqueness: true 
-
-
+  validates :email, uniqueness: true
 end
