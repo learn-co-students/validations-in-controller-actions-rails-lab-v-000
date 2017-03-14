887d51520d8978e6b2f6c55b929da9e0ba420b5e
Scott Bewick <bewick.scott@gmail.com>
2017-03-14 18:44:47 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209004521) do
+ActiveRecord::Schema.define(version: 20170314233919) do
 
   create_table "authors", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "email"
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.string   "category"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "category"
+    t.text   "content"
   end
 
 end
