4e9a120d05e64269d37ff094379694083b6f0e99
umohm1 <umohm12@gmail.com>
2017-04-23 21:51:35 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 100}
-  validates :category, inclusion: {in: %w(Fiction Non-Ficion)}
+  validates :Category, inclusion: {in: %w(Fiction Non-Ficion)}
 end
