b2da794394635718dd2e363bb325005c524d0907
Feiran Wang <wangfeiranpku@163.com>
2017-05-06 02:41:37 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,14 +4,19 @@ class PostsController < ApplicationController
   def show
   end
 
+
+
   def edit
   end
 
   def update
+    @post1=Post.new(post_params)
+    if @post1.valid?
     @post.update(post_params)
-
     redirect_to post_path(@post)
+  else render :edit
   end
+end
 
   private
 
