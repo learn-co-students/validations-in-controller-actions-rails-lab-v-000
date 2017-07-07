251712bea94f04762be6e946d8b03cba1db62c06
Matteo Piccini <gnappo.1@live.it>
2017-07-07 08:40:24 +0000

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ActiveRecord::Base
+  validates_presence_of :name, presence: true
+  validates :email, format: { with: /@/ }, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 60 }
 end
