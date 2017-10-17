d2d8be51b68eea874c4498d9fd54d7b0207d9e25
Archana Burra <burra.archana@gmail.com>
2017-10-17 19:12:18 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "Must be fiction or non-fiction" }
+  validates :content, length: { minimum: 100 }
 end
