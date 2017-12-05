b16d6958f7283a3d980cce92183a7fa973bdf9e6
Christian Egglin <cegglin@conncoll.edu>
2017-12-05 17:44:35 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, inclusion: {in: %w(Fiction fiction Non-Fiction non-fiction)}
+  validates :content, length: {minimum: 100}
 end
