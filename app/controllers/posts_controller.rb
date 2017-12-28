79c99d35447fe2c715cad38b85e8f16428be4626
dbfarms <matt.hunger@gmail.com>
2017-12-28 15:21:05 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,11 +6,17 @@ class PostsController < ApplicationController
 
   def edit
   end
-
+  
   def update
     @post.update(post_params)
 
-    redirect_to post_path(@post)
+    if @post.valid?
+      @post.save 
+      redirect_to post_path(@post)
+    else 
+      
+      render :edit
+    end 
   end
 
   private
