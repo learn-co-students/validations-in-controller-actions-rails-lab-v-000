db1a7646a3720d438c4b15e13a2134f09677f0aa
Jess Mitchell <jessicamitch42@gmail.com>
2018-01-31 18:25:32 +0000

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+  # 1. The name cannot be blank
+  validates :name, presence: true
+  # 1. The e-mail is unique
+  validates :email, uniqueness: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+  # 1. The title cannot be blank
+  validates :title, presence:true
+  # 1. The category is either `"Fiction"` or `"Non-Fiction"`
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  # 1. The content is at least 100
+  validates :content, length: {minimum: 100}
 end
