1424d2d01d2d0b46be055b0fd05ce8ca4e074e65
rh24 <ms.rebeccahong@gmail.com>
2018-03-02 16:50:25 -0500

    Refactor post#update controller action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    post = Post.new(post_params)
-    if post.valid?
-      @post.update(post_params)
+    if @post.update(post_params)
       redirect_to post_path(@post)
     else
       render :edit
