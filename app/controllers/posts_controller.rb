cf02060b24cda3ce7107a9b9bae57f3ff52636ed
Benny Hudson Hung <wakenightryu@gmail.com>
2018-03-02 19:21:16 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,10 +7,26 @@ class PostsController < ApplicationController
   def edit
   end
 
+  def create
+    @post.new(post_params)
+
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
   def update
     @post.update(post_params)
 
-    redirect_to post_path(@post)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
