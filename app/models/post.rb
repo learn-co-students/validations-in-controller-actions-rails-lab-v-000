99bccbe059f72bc918a678d22eb3c977a6534040
Jordan Kline <jordan.kline@gmail.com>
2018-03-04 11:35:13 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+	validates :content, length: { :minimum => 100}
+
+	# PostsController making invalid updates has an error for missing title
+	# PostsController making invalid updates has an error for too short content
+	# PostsController making invalid updates has an error for invalid category
+	# PostsController making invalid updates controller actions does not update
+	# PostsController making invalid updates controller actions renders the form again
+
 end
