ff8dac0b0749966c9ea79578fa4f5292a05c18ec
Naomi Dennis <njdennis94@gmail.com>
2018-04-16 01:17:46 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, presence: true, length: {minimum: 10}
+  validates :category, presence: true, length: {minimum: 10}
 end
