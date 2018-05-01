1e04ddc201a1131b777bef9d336fe257bd2c0fc8
Aubrey Neimeier <neimeier.1@osu.edu>
2018-05-01 16:16:14 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  #%w is notation that allows an array of strings that are delimited with spaces instead of quoutes and comma
+  # ex %w(foo bar) is equivalent to ["foo", "bar"]
+  validates :content, length: { minimum: 100 }
+
 end
