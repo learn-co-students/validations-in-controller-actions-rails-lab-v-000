723221052d24c001c46db7ff95c77903f2b3b27a
roitblatari <roitblatari@gmail.com>
2018-05-02 22:18:53 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,13 @@
 class Post < ActiveRecord::Base
-  validate :post_title
+  # validate :post_title
+  validates :title, presence: true
   validates :category, inclusion: { in: %w(Fiction  Non-Fiction)}
   validates :content, length: { minimum: 100 }
-  
-  def post_title
-    # binding.pry
-    if !self.title
-      self.errors.add(:title, "needs to be a title")
-    end
-  end
+
+  # def post_title
+  #   # binding.pry
+  #   if !self.title
+  #     self.errors.add(:title, "needs to be a title")
+  #   end
+  # end
 end
