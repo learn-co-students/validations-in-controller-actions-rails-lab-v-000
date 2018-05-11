427ccf5856b02eb9affb7ce361f3b2384125147f
github username <github email address>
2018-05-11 14:23:32 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,13 +4,34 @@ class AuthorsController < ApplicationController
   end
 
   def new
+      # @author = Author.new
+
   end
 
   def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.create(name: params[:name], email: params[:email])
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    @author = Author.find(params[:id])
+    if @author.valid?
+      @author.update(name: params[:name], email: params[:email])
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
+  end
+
 
   private
 
@@ -18,3 +39,32 @@ class AuthorsController < ApplicationController
     params.permit(:email, :name)
   end
 end
+
+# def new
+#   @author = Author.new
+# end
+
+# def create
+#   @author = Author.new(name: params[:author][:name], email: params[:author][:email])
+#   if @author.valid?
+#       @author.save
+#       redirect_to author_path(@author)
+#   else
+#       render :new
+#   end
+# end
+
+# def show
+#   @author = Author.find(params[:id])
+# end
+
+# def edit
+#   @author = Author.find(params[:id])
+# end
+
+# def update
+#   @author = Author.find(params[:id])
+#   @author.update(name: params[:author][:name], email: params[:author][:email])
+#   redirect_to author_path(@author)
+# end
+
