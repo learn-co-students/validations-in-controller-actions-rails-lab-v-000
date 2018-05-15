65f871d3a0bc169d99c09837b9d6c4767a1cccb1
Joanne Lee <vernapii@gmail.com>
2018-05-14 20:21:33 -0700

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,13 +4,32 @@ class AuthorsController < ApplicationController
   end
 
   def new
+    @author = Author.new(author_params)
   end
 
   def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
+
+  # def edit
+  #   @author = Author.find(params[:id])
+  # end
+  #
+  # def update
+  #   @author = Author.update(author_params)
+  #   if @author.valid?
+  #     @author.save
+  #     redirect_to author_path(@author)
+  #   else
+  #     render :new
+  #   end
+  # end
 
   private
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
@@ -5,12 +6,18 @@ class PostsController < ApplicationController
   end
 
   def edit
+  set_post!
   end
 
   def update
+    set_post!
     @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   private
