d1f9b1af13187a01554ce7856396b115ef058de8
kristinreddington <kristinreddington@gmail.com>
2018-05-22 14:07:13 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ActiveRecord::Base
+  validates :name, presence:  {message: "Must have name" }
+  validates :email, uniqueness: {message: "Must have unique name"}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: {message: "Must have title"}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validates :content, length: {minimum: 100}
 end
