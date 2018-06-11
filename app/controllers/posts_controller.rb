1e81cf3680b2affee75e595bbfb73a85e639b5be
Gingertonic <bethmschofield@gmail.com>
2018-06-11 12:38:50 +0930

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
+  def index
+    @posts = Post.all
+  end
 
   def show
   end
@@ -8,9 +11,12 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    # @post = Post.new(post_params)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get "/authors", to: "authors#index"
   get "/authors/new", to: "authors#new", as: "new_author"
   get "/authors/:id", to: "authors#show", as: "author"
   post "/authors", to: "authors#create"
 
+  get "/posts", to: "posts#index"
   get "/posts/:id", to: "posts#show", as: "post"
   get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
   patch "/posts/:id", to: "posts#update"
