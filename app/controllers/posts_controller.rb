60f01ba3d06e0ad5f8c73199e541e723ed609643
Emi Ko <emi8emi@gmail.com>
2018-07-02 12:24:13 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,8 @@ class PostsController < ApplicationController
   end
 
   def update
-    @p = Post.new(post_params)
-    if @p.valid?
-      @post.update(post_params)
+
+    if @post.update(post_params)
       redirect_to post_path(@post)
     else
       render :edit
