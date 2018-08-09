7d1a5ec495c3cf2ef55a8e557e79d20a18ca506d
arye-dov-eidelman <aryedov.e@gmail.com>
2018-08-09 18:11:02 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,23 +8,12 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.new(author_params)
-    if @author.valid?
-      @author.save
-      redirect_to author_path(@author)
-    else
-      # binding.pry
-      render '/authors/new'
-    end
+    @author.save ? (redirect_to @author) : (render 'authors/new')
   end
 
   private
 
   def author_params
-    # binding.pry
-    if params[:author]
-      params.require(:author).permit(:email, :name)
-    else
-      params.permit(:email, :name)
-    end
+    params.permit(:email, :name)
   end
 end
