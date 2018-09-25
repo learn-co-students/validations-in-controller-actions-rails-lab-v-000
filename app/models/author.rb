e9627b3c787ca44e12f044c1ca23712c7296c21e
emilywelsch <emwelsch@gmail.com>
2018-09-25 16:15:16 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ActiveRecord::Base
+  validates :name, length: {minimum: 1}, presence: true
+  validates_uniqueness_of :email
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, length: {minimum: 1}, presence: true
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates :content, length: {minimum: 100}
 end
