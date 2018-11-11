9b0411ea2f621fd5949169a5fd867e7e3dd0b357
atxrenegade <harleighabel@gmail.com>
2018-11-11 01:06:56 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,14 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
+	  set_post!
+	  if @post.update(post_params)
+	    redirect_to(@post)
+	  else
+	    render "edit"
+	  end
+  	end
 
-    redirect_to post_path(@post)
-  end
 
   private
 
