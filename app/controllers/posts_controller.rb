81925a15029338c0bade0e035d816e1a617a5bc4
amitnyc83 <amit_nyc@live.com>
2018-11-21 16:52:13 -0500

    all test passing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,35 @@
 class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
+
+
   def show
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_poath(@post)
+    else
+
+      render :new
+    end
+  end
+
 
   def edit
   end
 
   def update
     @post.update(post_params)
-
+    if @post.valid?
+      @post.save
     redirect_to post_path(@post)
+  else
+     render :edit
   end
+end 
 
   private
 
