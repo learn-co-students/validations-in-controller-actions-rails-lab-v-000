463b0dedca202df12d07961bb1f23889bacb845b
Robert Snyder <r.snyder49@gmail.com>
2018-12-03 22:30:55 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,14 +7,26 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.create(author_params)
+    if @author.valid?
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
 
   private
-
+  
+  def update
+    if @post.update(author_params)
+      redirect_to author(@post)	    
+    else
+      render :edit
+    end
+  end	  
+  
   def author_params
     params.permit(:email, :name)
   end
+  
 end
