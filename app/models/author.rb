9177d8e03a2b0897b3995a1fa034914ad7964a02
Elizabeth <efkarst@outlook.com>
2018-12-27 16:14:25 -0600

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,12 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  validate :has_valid_email
+
+  def has_valid_email
+    unless email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      errors.add(:email, "is not an email")
+    end
+  end
+
 end
