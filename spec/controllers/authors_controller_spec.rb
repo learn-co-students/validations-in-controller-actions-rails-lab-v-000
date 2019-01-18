43157d3127df94989da3a69f906360121b8d7856
Harley Rayner <harleymteden@gmail.com>
2019-01-18 20:00:11 +0000

    Done.

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe AuthorsController do
 
   describe "showing an author" do
     it "shows an author" do
-      get :show, id: jeff.id
+      get :show, params: {id: jeff.id}
       expect(jeff_found.id).to eq(jeff.id)
     end
   end
@@ -14,7 +14,7 @@ RSpec.describe AuthorsController do
   describe "creating a valid author" do
     let(:bro_found) { Author.find_by(name: "S. Bro") }
 
-    before { post :create, name: "S. Bro", email: "bro@sbahj.info" }
+    before { post :create, params:{ name: "S. Bro", email: "bro@sbahj.info"} }
 
     it "creates successfully" do
       expect(bro_found).to be_a(Author)
