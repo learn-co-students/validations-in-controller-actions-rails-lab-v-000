756fa0fdc9565a44f56f7b91fef4ac3553e5b958
Daniel Goldberg <danielgoldberg@Daniels-MBP.fios-router.home>
2019-02-03 21:45:31 -0500

    Add validations to model and failed form render statements to controller

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ActiveRecord::Base
+
+	validates :name, presence: true
+	validates :email, presence: true
+	# validates_length_of :phone_number, minimum: 10, maximum: 10
+	validates_uniqueness_of :email
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+
+	validates :title, presence: true
+	validates_length_of :content, minimum: 250
+	# validates_length_of :summary, maximum: 250
+	validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+	# validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+	# validate :check_for_clickbait
+
+	# def check_for_clickbait
+	# 	clickbait_phrases = ["Won't Believe", "Secret", "Guess", /Top\s\d/]
+	# 	if title && !clickbait_phrases.any?{|substr|title.scan(substr).count > 0}
+	# 		errors.add(:title, "Must include clickbait phrase")
+	# 	end
+	# end
+
 end
