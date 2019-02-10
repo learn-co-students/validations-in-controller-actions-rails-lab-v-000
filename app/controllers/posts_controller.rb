8234e1a909868e7c104a07cd6b3b217f58f9088e
Erin Green <erin.y.green@gmail.com>
2019-02-10 19:23:35 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,19 @@
 class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
+  
+  def new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+  end
+  
   def show
   end
 
@@ -8,6 +21,7 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
     if @post.valid? 
       @post.update(post_params)
       redirect_to post_path(@post)
