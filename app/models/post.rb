050ea01c1d4d8aa1cbf02052d23af6f5d314604d
leothebeagle <a.kosber@gmail.com>
2019-05-07 14:56:13 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 100 }
+  validates :content, presence: true
+  validate :category_validation
+
+
+  def category_validation
+    categories = ["Fiction", "Non-Fiction"]
+
+    if categories.none? { |x| x.match category }
+      errors.add(:category, "Category must be Fiction or Non-Fiction")
+    end
+  end
 end
