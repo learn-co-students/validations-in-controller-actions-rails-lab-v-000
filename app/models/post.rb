17f9a307360d04aa6cbf599f10dacf498f6d541c
Diapause <diapause@protonmail.com>
2020-03-24 13:26:56 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, presence: true, inclusion: { in: ["Fiction", "Non-Fiction"]}
+  validates :content, presence: true, length: { minimum: 10 }
 end
