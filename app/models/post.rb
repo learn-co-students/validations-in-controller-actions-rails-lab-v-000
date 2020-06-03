daf006e48ac51226c93e9ab25460e90390e7c8a0
Mitchell Beebe <beebe.mitch@gmail.com>
2020-06-03 19:36:48 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+    include ActiveModel::Validations
+    validates_with PostValidator
+    validates :title, length: { minimum: 1 }
+    validates :content, length: { minimum: 200 }
 end
