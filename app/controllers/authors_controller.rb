f5d0531069cd625847132ad12445e24303144dc2
Eric Johnson <ericjohnson0216@icloud.com>
2020-10-28 23:15:20 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,6 @@
 class AuthorsController < ApplicationController
+  
+
   def show
     @author = Author.find(params[:id])
   end
@@ -7,7 +9,7 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
+    @author = Author.new(author_params)
 
     if @author.valid?
       @author.save
